c18582eec92b56e1011bd84eb62df52539f275f4
JohnJaeger <jmjaeger10@gmail.com>
2024-05-07 22:41:23 -0400

    Rework javascript logic to incorporate ui

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
-alert("Hello! Lets play a game. Best of 5.");
-
 let userScore = 0;
 let computerScore = 0;
+let userChoice = "";
+let computerChoice = "";
+
+const pageTitle = document.querySelector("#page-title");
+const playerScoreDisplay = document.querySelector("#player-score-display");
+const computerScoreDisplay = document.querySelector("#computer-score-display");
+const playerCurrentChoiceDisplay = document.querySelector("#player-current-choice-display");
+const computerCurrentChoiceDisplay = document.querySelector("#computer-current-choice-display");
+const playerRock = document.querySelector("#player-rock");
+const playerPaper = document.querySelector("#player-paper");
+const playerScissors = document.querySelector("#player-scissors");
+const computerRock = document.querySelector("#computer-rock");
+const computerPaper = document.querySelector("#computer-paper");
+const computerScissors = document.querySelector("#computer-scissors");
 
 function getComputerChoice(){
     let randomNumber = (Math.random() * 100);
@@ -15,67 +27,143 @@ function getComputerChoice(){
     }
 }
 
-function playGame(){
-    let userChoice = prompt("Rock, paper, or scissors?", "").toLowerCase();
+function updateScoreboard(){
+    pageTitle.textContent = "Rock Paper Scissors";
 
-    let computerChoice = getComputerChoice();
+    playerScoreDisplay.textContent = "Player Score: " + userScore;
+    computerScoreDisplay.textContent = "Computer Score: " + computerScore;
+}
 
-    alert("I choose " + computerChoice + ".");
+function updateChoice(){
+    if (userChoice === "rock"){
+        playerCurrentChoiceDisplay.style.paddingTop = "15px";
+        playerCurrentChoiceDisplay.style.paddingBottom = "0px";
+        playerCurrentChoiceDisplay.textContent = "🪨";
+    } else if (userChoice === "paper"){
+        playerCurrentChoiceDisplay.style.paddingTop = "0px";
+        playerCurrentChoiceDisplay.style.paddingBottom = "15px";
+        playerCurrentChoiceDisplay.textContent = "📃";
+    } else if (userChoice === "scissors"){
+        playerCurrentChoiceDisplay.style.paddingTop = "5px";
+        playerCurrentChoiceDisplay.style.paddingBottom = "10px";
+        playerCurrentChoiceDisplay.textContent = "✂️";
+    }
 
+    if (computerChoice === "rock"){
+        computerCurrentChoiceDisplay.style.paddingTop = "15px";
+        computerCurrentChoiceDisplay.style.paddingBottom = "0px";
+        computerCurrentChoiceDisplay.textContent = "🪨";
+    } else if (computerChoice === "paper"){
+        computerCurrentChoiceDisplay.style.paddingTop = "0px";
+        computerCurrentChoiceDisplay.style.paddingBottom = "15px";
+        computerCurrentChoiceDisplay.textContent = "📃";
+    } else if (computerChoice === "scissors"){
+        computerCurrentChoiceDisplay.style.paddingTop = "5px";
+        computerCurrentChoiceDisplay.style.paddingBottom = "10px";
+        computerCurrentChoiceDisplay.textContent = "✂️";
+    }
+}
+
+function playRound(){
     if (userChoice === "rock" && computerChoice === "rock"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + computerScore);
+        updateChoice();
     } else if (userChoice === "rock" && computerChoice === "paper"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + ++computerScore);
+        ++computerScore;
+        updateScoreboard();
+        updateChoice();
     } else if (userChoice === "rock" && computerChoice === "scissors"){
-        alert('User Score: ' + ++userScore + '\nComputer Score: ' + computerScore);
+        ++userScore;
+        updateScoreboard();
+        updateChoice();
     }
 
     if (userChoice === "paper" && computerChoice === "rock"){
-        alert('User Score: ' + ++userScore + '\nComputer Score: ' + computerScore);
+        ++userScore;
+        updateScoreboard();
+        updateChoice();
     } else if (userChoice === "paper" && computerChoice === "paper"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + computerScore);
+        updateChoice();
     } else if (userChoice === "paper" && computerChoice === "scissors"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + ++computerScore);
+        ++computerScore;
+        updateScoreboard();
+        updateChoice();
     }
 
     if (userChoice === "scissors" && computerChoice === "rock"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + ++computerScore);
+        ++computerScore;
+        updateScoreboard();
+        updateChoice();
     } else if (userChoice === "scissors" && computerChoice === "paper"){
-        alert('User Score: ' + ++userScore + '\nComputer Score: ' + computerScore);
+        ++userScore;
+        updateScoreboard();
+        updateChoice();
     } else if (userChoice === "scissors" && computerChoice === "scissors"){
-        alert('User Score: ' + userScore + '\nComputer Score: ' + computerScore);
+        updateChoice();
     }
 
-    if (userScore === 3){
+    if (userScore === 3 || computerScore === 3){
         endGame();
-    } else if (computerScore === 3){
-        endGame();
-    } else {
-        playGame();
     }
 }
 
 function endGame(){
     if (userScore === 3){
-        alert("You Win!");
+        pageTitle.textContent = "You Win!";
+        userScore = 0;
+        computerScore = 0;
     } else if (computerScore === 3){
-        alert("I Win!");
+        pageTitle.textContent = "I Win!";
+        userScore = 0;
+        computerScore = 0;
     }
 }
 
-playGame();
+updateScoreboard();
 
-/*
+const playerChoices = document.querySelector("#player-choices-display");
 
-For now, remove the logic that plays exactly five rounds.
+playerChoices.addEventListener("click", function(event){
+    switch(event.target.id){
+        case "player-rock":
+            userChoice = "rock";
+            break;
+        case "player-paper":
+            userChoice = "paper";
+            break;
+        case "player-scissors":
+            userChoice = "scissors";
+            break;
+    }
 
-Create three buttons, one for each selection.
+    computerChoice = getComputerChoice();
 
-Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. 
-    (you can keep the console.logs for this step)
+    playRound();
+});
 
-Add a div for displaying results and change all of your console.logs into DOM methods.
+playerChoices.addEventListener("mouseover", function(event){
+    switch(event.target.id){
+        case "player-rock":
+            playerRock.style.boxShadow = "2px 2px 4px black";
+            break;
+        case "player-paper":
+            playerPaper.style.boxShadow = "2px 2px 4px black";
+            break;
+        case "player-scissors":
+            playerScissors.style.boxShadow = "2px 2px 4px black";
+            break;
+    }
+});
 
-Display the running score, and announce a winner of the game once one player reaches 5 points.
-
-*/
+playerChoices.addEventListener("mouseout", function(event){
+    switch(event.target.id){
+        case "player-rock":
+            playerRock.style.boxShadow = "0px 0px 0px black";
+            break;
+        case "player-paper":
+            playerPaper.style.boxShadow = "0px 0px 0px black";
+            break;
+        case "player-scissors":
+            playerScissors.style.boxShadow = "0px 0px 0px black";
+            break;
+    }
+});
